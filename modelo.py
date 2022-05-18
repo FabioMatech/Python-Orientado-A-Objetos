c7337567b9a89b8bc02e802d87bb026bda0aa443
Fabio <clubeteczagem@gmail.com>
2022-05-18 08:48:11 -0300

    Atualização da classe Playlist, herdando list

--- modelo.py
+++ modelo.py
@@ -39,13 +39,11 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} likes'
 
-class Playlist:
+class Playlist(list):
     def __init__(self, nome, programas):
-        self._nome = nome
-        self.programas = programas
+        self.nome = nome
+        super().__init__(programas)
 
-    def tamanho(self):
-        return len(self.programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -64,7 +62,11 @@ demolidor.dar_likes()
 
 
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
-palylist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
+playlist_fim_de_semana = Playlist("Fim de semana", filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)} ')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Demolidor está na playlist ?  {demolidor in playlist_fim_de_semana}')
