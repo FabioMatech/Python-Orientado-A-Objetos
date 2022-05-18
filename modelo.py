27d1099696998cceadcc41d71302cff469bf60c3
Fabio <clubeteczagem@gmail.com>
2022-05-18 09:11:06 -0300

    Tirando a herança, não herda mais a classe list.

--- modelo.py
+++ modelo.py
@@ -39,10 +39,18 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -64,9 +72,8 @@ demolidor.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist("Fim de semana", filmes_e_series)
 
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana)} ')
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)} ')
 
-for programa in playlist_fim_de_semana:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
 
-print(f'Demolidor está na playlist ?  {demolidor in playlist_fim_de_semana}')
